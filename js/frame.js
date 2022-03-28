1ba13ece5cc3d904009be0006f61e72ecc8a27b5
josekutty <josekuttyshaju98@gmail.com>
2022-03-28 16:53:50 +0530

    loader

--- js/frame.js
+++ js/frame.js
@@ -10,16 +10,31 @@ const tall = Math.max(document.documentElement.clientHeight || 0, window.innerHe
 
 window.addEventListener('resize', trans);
 
+const docx = document.documentElement;
 function trans(){
     bt = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-    ht = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-    const docx = document.documentElement;
+    ht = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);;
     docx.style.setProperty('--dim1',Math.min(1,bt/wide));
     docx.style.setProperty('--dim2',Math.min(1,ht/tall));
  
 }
 
-  window.addEventListener('load', function () {
-    document.body.classList.add('loaded');
-  })
-  
+
+function setIntervalX(callback, delay, repetitions) {
+  var x = 0;
+  var intervalID = window.setInterval(function () {
+
+     callback();
+
+     if (++x === repetitions) {
+         window.clearInterval(intervalID);
+     }
+  }, delay);
+}
+z = 0;
+
+setIntervalX(function () {
+  z += 0.01
+  docx.style.setProperty('--loader',z);
+}, 2, 100);
+
