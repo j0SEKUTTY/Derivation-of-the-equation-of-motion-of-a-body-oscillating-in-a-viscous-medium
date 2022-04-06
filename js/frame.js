e9adbc6a85ec93df59046b1569c35b96f0a5ce18
josekutty <josekuttyshaju98@gmail.com>
2022-04-06 17:12:40 +0530

    cc image attribution

--- js/frame.js
+++ js/frame.js
@@ -1,11 +1,11 @@
 
-document.getElementById('page_thumbnails_container').innerHTML = '<a class="thumbnail" href="reality.html"><img src="images/swingingGirl.webp" Title ="Swing by Natasha Sinegina"></a><a class="thumbnail" href="idealisation.html"><img  src="images/swinging_pendulum.webp"></a><a class="thumbnail" href="geometric_a.html"><img  src="images/geometric.webp"></a><a class="thumbnail" href="algebra.html"><img  src="images/algebra.webp"></a>';
+document.getElementById('page_thumbnails_container').innerHTML = '<a class="thumbnail" href="reality.html"><img src="images/swingingGirl.webp"></a><a class="thumbnail" href="idealisation.html"><img  src="images/swinging_pendulum.webp"></a><a class="thumbnail" href="geometric_a.html"><img  src="images/geometric.webp"></a><a class="thumbnail" href="algebra.html"><img  src="images/algebra.webp"></a>';
 
-document.getElementById('page_names').innerHTML ='<text>Reality</text><text>Idealisation</text><text>Geometric</text><text>Algebra</text>'
+document.getElementById('page_names').innerHTML ='<text>Reality</text><text>Idealisation</text><text>Geometric</text><text>Algebra</text>';
 
-document.getElementById('progress').innerHTML= '<div id ="bar"></div><img src="images/arrow.webp">'
+document.getElementById('progress').innerHTML= '<div id ="bar"></div><img src="images/arrow.webp">';
 
-document.getElementById('navigator').innerHTML='<a id="left" class="nav" ><img src="images/nav_icon.webp"></a><a id="right" class="nav"><img src="images/nav_icon.webp"></a>'
+document.getElementById('navigator').innerHTML='<a id="left" class="nav" ><img src="images/nav_icon.webp"></a><a id="right" class="nav"><img src="images/nav_icon.webp"></a>';
 
 const wide = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 const tall = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
